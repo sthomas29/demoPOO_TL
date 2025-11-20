74e67bfb51d0358ae5af3ab05513de3e011181ae
Stéphane THOMAS <thomas91@free.fr>
2025-11-20 06:47:08 -0300

    Enregistrement du modèle Voiture

---
+++ models/Voiture.py
@@ -0,0 +1,13 @@
+class Voiture :
+    def __init__(self, marque, modele, kilometrage=0) :
+        self.modele = modele
+        self.marque = marque
+        self.kilometrage = kilometrage
+
+        print("Je crée une instance de voiture.")
+
+"""
+Caractéristiques ==> Attributs
+
+Capacités ==> Méthodes (fonctions du langage)
+"""
