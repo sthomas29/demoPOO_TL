61dfd829572f76974ee5a9eb16c16204c69a1f27
Stéphane THOMAS <thomas91@free.fr>
2025-11-21 07:46:38 -0300

    Update : Simplification de l'héritage multiple en redéfinissant l'arbre Vehicule > Voiture > Monospace > Scenic

--- models/Monospace.py
+++ models/Monospace.py
@@ -1,5 +1,8 @@
-class Monospace:
+from models.Voiture import Voiture
+
+
+class Monospace (Voiture):
 
 
     def partir_en_vacances(self):
-        print("Partir en vacances en famille !")
+        print("Partir en vacances en famille en MONOSPACE !")
--- models/Scenic.py
+++ models/Scenic.py
@@ -2,7 +2,7 @@ from models.Monospace import Monospace
 from models.Voiture import Voiture
 
 
-class Scenic(Monospace, Voiture) :
+class Scenic(Monospace) :
 
     # Constructeur de Scenic
     def __init__(self, marque, modele, option):
@@ -19,6 +19,9 @@ class Scenic(Monospace, Voiture) :
     def rouler(self, kilometrage):
         print ("Je roule avec un scenic et son coffre de toit")
         super().rouler(kilometrage)
+
+    def partir_en_vacances(self):
+        print("Partir en vacances en famille en SCENIC - MONOSPACE - VOITURE !")
 
     def __str__(self):
         return f"Scenic - {super().__str__()} - {self.__option}"
