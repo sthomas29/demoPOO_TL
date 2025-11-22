c4ab283e0ca4487d2ec69c211566a74435f33e26
Stéphane THOMAS <thomas91@free.fr>
2025-11-22 06:46:07 -0300

    Optimisation du code : Ajout de commentaires explicatifs + Réorganisation des définitions de requêtes SQL

--- demoSql.py
+++ demoSql.py
@@ -18,26 +18,33 @@ Application Base de Données :
 import sqlite3
 from datetime import date
 
+#DDL Data Definition Language
 REQ_DROP_TABLE_USERS="DROP TABLE IF EXISTS users"
 REQ_CREATE_USERS="""CREATE TABLE IF NOT EXISTS users (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         name TEXT NOT NULL UNIQUE,
                         age INTEGER
                  )"""
+#DML Data Manipulation Language
+REQ_INSERT_USER="INSERT INTO users (name, age) VALUES (?, ?)"
+REQ_INSERT_USER_ONLY_NAME ="INSERT INTO users (name) VALUES (?)"
 
 REQ_SELECT_ALL="SELECT * FROM users"
 REQ_SELECT_BY_NAME="SELECT * FROM users WHERE name = ?"
 REQ_SELECT_BY_ID="SELECT * FROM users WHERE id = ?"
 REQ_SELECT_BY_AGE="SELECT * FROM users WHERE age = ?"
-REQ_UPDATE_AGE="UPDATE users SET age = ? WHERE name = ?"
-REQ_DELETE_USER="DELETE FROM users WHERE name = ?"
-REQ_INSERT_USER="INSERT INTO users (name, age) VALUES (?, ?)"
-REQ_INSERT_USER_ONLY_NAME ="INSERT INTO users (name) VALUES (?)"
 
+REQ_UPDATE_AGE="UPDATE users SET age = ? WHERE name = ?"
+REQ_UPDATE_NAME="UPDATE users SET name = ? WHERE name = ?"
+
+REQ_DELETE_USER="DELETE FROM users WHERE name = ?"
+
+# Récupère une connexion à la base de données
 def get_connection():
     conn = sqlite3.connect("demoBDD_TL.db")
     return conn
 
+# Calcule l'âge du personnage en fonction de la date du jour
 def get_age(year,month,day):
     date_ref = date(year,month,day)  # exemple
     aujourdhui = date.today()
@@ -47,6 +54,7 @@ def get_age(year,month,day):
     )
     return age
 
+# Exécution d'une requête
 def execute_query(query, params=()):
 
     #print (f"Type : {type(params)}")
@@ -75,7 +83,6 @@ execute_query(REQ_CREATE_USERS)
 
 # C de CRUD (création d'un enregistrement dans la table)
 print("--- C de CRUD ---")
-
 execute_query(REQ_INSERT_USER, ("Minnie", get_age(1928,11,18)))
 execute_query(REQ_INSERT_USER, ("Mickey", get_age(1928,11,18)))
 execute_query(REQ_INSERT_USER, ("Donald", get_age(1934,6,9)))
@@ -86,14 +93,11 @@ execute_query(REQ_INSERT_USER, ("Picsou",get_age(1867,7,8)))
 print("--- R de CRUD ---")
 resultat = execute_query(REQ_SELECT_ALL)
 
-# for user in resultat:
-#     print(f"{user[1]} - {user[2]} ({user[0]})")
-
 # U de CRUD
 execute_query(REQ_UPDATE_AGE, (get_age(1930, 8,18),"Pluto"))
-
 execute_query(REQ_SELECT_BY_NAME, ("Pluto",))
 
+# Ajout d'un personnage, le méchant ! :-)
 execute_query(REQ_INSERT_USER, ("Pat Hibulaire", get_age(1925,2,15)))
 execute_query(REQ_SELECT_BY_NAME, ("Pat Hibulaire",))
 
